6890770179a98b43cbebdd6db5242cf57225943a
David <davewil1973@gmail.com>
2024-02-26 00:10:53 +0000

    _

--- lib/json-fetcher.ex
+++ lib/json-fetcher.ex
@@ -6,13 +6,18 @@ defmodule JsonFetcher do
   end
 
   def get_json2() do
-    data = Req.get!("https://upredict.co.uk/games/39-2023-26").body
+    data = Req.get!("https://upredict.co.uk/games/39-2023-26", decode_json: [keys: :atoms]).body
 
-    Map.fetch(data, "player")
-    Map.fetch(data, "matchdays")
+    %{player: player, matchdays: matchdays} = data
+    %{name: homeTeam} = (hd matchdays.requested.fixtures).homeTeam
 
-    with {:ok, player} <- Map.fetch(data, "player"),
-         {:ok, matchdays} <- Map.fetch(data, "matchdays"),
-         do: "#{player["isPlaying"]} " <> "#{matchdays["requested"]["id"]}"
+    "#{player.isPlaying} " <> "#{homeTeam}"
+
+    # Map.fetch(data, :player)
+    # Map.fetch(data, :matchdays)
+
+    # with {:ok, player} <- Map.fetch(data, :player),
+    #      {:ok, matchdays} <- Map.fetch(data, :matchdays),
+    #      do: "#{player.isPlaying} " <> "#{matchdays.requested.id}"
   end
 end
